48eafd55077aabe5002cc6e9554dfa3c4320c67f
AVAndrianov <a.v.andrianov33@gmail.com>
2025-08-06 16:33:13 +0300

    update chapter6

--- src/main/java/ru/task/java/chapter6/task21/MethodForConstructingArrays.java
+++ src/main/java/ru/task/java/chapter6/task21/MethodForConstructingArrays.java
@@ -1,36 +1,23 @@
 package ru.task.java.chapter6.task21;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MethodForConstructingArrays {
 
-    //Используя аннотацию @SafeV arargs, напишите метод, позволяющий строить
-    //массивы обобщенных типов, как в следующем примере:
-    //Lists.swapHelper(result, 0, 1);
-    //}
-    //List<String>[ ] result = Arrays.<List<String»construct (10) ;
-    //Устанавливает результат в списке типа List<String>[] длиной 10
     public static void main(String[] args) {
-
-    }
-
-    public void metod() {
-        List<String>[] result = Arrays.construct(10);
-
+        List<String>[] result = construct(List.class, 10);
         for (int i = 0; i < result.length; i++) {
             result[i] = new ArrayList<>();
         }
-
         result[0].add("Hello");
         result[1].add("World");
     }
 
-    public static class Arrays {
-        public static <T> T[] construct(int size) {
-            @SuppressWarnings("unchecked")
-            T[] arr = (T[]) new Object[size];
-            return arr;
-        }
+    public static <T> T[] construct(Class<T> clazz, int size) {
+        @SuppressWarnings("unchecked")
+        T[] arr = (T[]) Array.newInstance(clazz, size);
+        return arr;
     }
 }
