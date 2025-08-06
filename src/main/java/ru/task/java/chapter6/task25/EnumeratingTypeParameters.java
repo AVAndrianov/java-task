f6755f8af21cbdaf9916ae071a0dcb7047d6df77
AVAndrianov <a.v.andrianov33@gmail.com>
2025-08-06 17:08:11 +0300

    update chapter6

--- src/main/java/ru/task/java/chapter6/task25/EnumeratingTypeParameters.java
+++ src/main/java/ru/task/java/chapter6/task25/EnumeratingTypeParameters.java
@@ -1,6 +1,16 @@
 package ru.task.java.chapter6.task25;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class EnumeratingTypeParameters {
+    public static void main(String[] args) throws NoSuchMethodException {
+        Method m = String.class.getDeclaredMethod("newStringUTF8NoRepl", byte[].class, int.class, int.class);
+        genericDeclaration(m);
+    }
 
-
+    public static String genericDeclaration(Method m) {
+        Arrays.stream(m.getTypeParameters()).forEach(System.out::println);
+        return "";
+    }
 }
