456381915d53cf7c7012f9ede0db8930c6890251
AVAndrianov <a.v.andrianov33@gmail.com>
2025-08-06 19:57:45 +0300

    update chapter6

--- src/main/java/ru/task/java/chapter6/task14/ImprovedVersionCloseAllMethod.java
+++ src/main/java/ru/task/java/chapter6/task14/ImprovedVersionCloseAllMethod.java
@@ -1,31 +1,26 @@
 package ru.task.java.chapter6.task14;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collection;
 
 public class ImprovedVersionCloseAllMethod {
 
-
-    public static <E extends AutoCloseable> void closeAll(ArrayList<E> elems)
-            throws Exception {
-        for (E elem : elems) elem.close();
-    }
-
-    static class MyResource implements AutoCloseable {
-        @Override
-        public void close() throws IOException {
-            throw new IOException();
-//            System.out.println("Closing MyResource");
+    public static void closeAll(Collection<? extends AutoCloseable> resources) throws Exception {
+        Exception primaryException = null;
+        for (AutoCloseable resource : resources) {
+            try {
+                if (resource != null) {
+                    resource.close();
+                }
+            } catch (Exception e) {
+                if (primaryException == null) {
+                    primaryException = e;
+                } else {
+                    primaryException.addSuppressed(e);
+                }
+            }
         }
-    }
-
-    static class MyCustomException extends IOException {
-        public MyCustomException(String message) {
-            super(message);
-        }
-
-        public MyCustomException(String message, Throwable cause) {
-            super(message, cause); // Вызываем конструктор суперкласса для связывания
+        if (primaryException != null) {
+            throw primaryException;
         }
     }
 }
